de56dbda38a7bd4477b554483b431bd3c6a6ebfe
Chi Yeung <yeun0007@umn.edu>
2020-01-20 15:49:16 -0700

    Update background interpretation value.
    Bug fix.

--- web/view/gonzalezThin.js
+++ web/view/gonzalezThin.js
@@ -100,7 +100,7 @@ GonzalezThin.prototype.deleteBorder = function() {
             
             // mark it green !
                this.dataDes.data[i] = 0;
-               this.dataDes.data[i+1] = 0;
+               this.dataDes.data[i+1] = 255;
                this.dataDes.data[i+2] = 0;
                this.dataDes.data[i+3] = 0;
          }
@@ -148,27 +148,35 @@ GonzalezThin.prototype.correctCount = function() {	// [out] meets 2<=N(p1)<=6 cr
 GonzalezThin.prototype.transition = function() {	// [out] meets S(p1)==1 criterior 0 to 1 transition
    var total=0;
    for(var i=0; i<this.listNeighbors.length-1; i++) {
-      if(this.listNeighbors[i]==0)
-         if(this.listNeighbors[i+1]!=0)
+      // NOT background color 0
+      if(this.listNeighbors[i]!=0)
+         // IS background color 0
+         if(this.listNeighbors[i+1]==0)
             total ++;
    }
-   if(this.listNeighbors[this.listNeighbors.length-1]==0)
-      if(this.listNeighbors[0]!=0)
-         total ++;
 
+   // NOT background color 0
+   if(this.listNeighbors[this.listNeighbors.length-1]!=0)
+   {
+      // IS background color 0
+      if(this.listNeighbors[0]==0)
+      total ++;
+   }
+      
    return (total==1)?true:false;
 }
 
 // pg 492 criterior (c, d) step 1
 GonzalezThin.prototype.step1CD = function() {
-   if(this.listNeighbors[this.P4]==0)
+   // NOT background color 0
+   if(this.listNeighbors[this.P4]!=0)
       return true;
 
-   if(this.listNeighbors[this.P6]==0)
+   if(this.listNeighbors[this.P6]!=0)
       return true;
 
-   if(this.listNeighbors[this.P2]==0&&
-      this.listNeighbors[this.P8]==0)
+   if(this.listNeighbors[this.P2]!=0&&
+      this.listNeighbors[this.P8]!=0)
       return true;
 
 	return false;
@@ -176,14 +184,15 @@ GonzalezThin.prototype.step1CD = function() {
 		
 // pg 492 criterior (c, d) step 2
 GonzalezThin.prototype.step2CD = function() {
-   if(this.listNeighbors[this.P2]==0)
+   // NOT background color 0
+   if(this.listNeighbors[this.P2]!=0)
       return true;
 
-   if(this.listNeighbors[this.P8]==0)
+   if(this.listNeighbors[this.P8]!=0)
       return true;
 
-   if(this.listNeighbors[this.P6]==0&&
-      this.listNeighbors[this.P4]==0)
+   if(this.listNeighbors[this.P6]!=0&&
+      this.listNeighbors[this.P4]!=0)
       return true;
 
    return false;
